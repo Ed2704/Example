2abbf221c82549216b0791a9e62d94d9559cc6bb
Eduard <egorov-ed@mail.ru>
2022-09-15 08:50:44 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a=1;
+int b=4;
+int c=8;
+int d=3;
+int e=5;
+
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+
+Console.Write("Максимум:");
+Console.WriteLine(max);
