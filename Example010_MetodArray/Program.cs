94a40cfd62a6f1545498bcfa926e286dc2dcf0e7
Eduard <egorov-ed@mail.ru>
2022-09-18 21:11:50 +0300

    example 010 metod array

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array ={2,5,3,556,2355,52,354,3,97,282,54};
+
+int n = array.Length;
+int find = 3;
+int index=0;
+
+while(index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
